c72dcf3ea97a2f4185fad2cfb20f049b117a2fd5
Tafsan-Mahmud <tafsanmahmudnobin090@gmail.com>
2025-09-01 22:58:39 +0600

    implementing server action signin

---
+++ apis/handleSignin.ts
@@ -0,0 +1,41 @@
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { uri } from '@/public/apiuri/uri'
+interface RegistrationFormData {
+    email: string;
+    password: string;
+}
+export const SigninAuth = async (data: RegistrationFormData, router: AppRouterInstance) => {
+    console.log(data, router)
+    // try {
+    //     const response = await fetch(`${uri}/merchant/register`, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(data),
+    //     });
+
+    //     const responseData = await response.json();
+    //     if (!response.ok) {
+    //         return (responseData.message || 'Network response was not ok');
+    //     }
+
+    //     if (responseData.status === 'SUCCESS') {
+    //         if (responseData.action === "showemailotp") {
+    //             sessionStorage.setItem('resdt', responseData.data)
+    //             sessionStorage.setItem('resml', responseData.email)
+    //             router.push(`/verify-account?redirection=otp_sending_registration_${responseData.email} `);
+    //         }
+    //     } else {
+    //         return responseData;
+    //     }
+    // } catch (error) {
+    //     // console.error('An error occurred during registration:', error);
+    //     if (error instanceof Error) {
+    //         return error.message;
+    //     } else {
+    //         return 'An unknown error occurred.';
+    //     }
+    // }
+}
+
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Ppts from "@/components/ppts"
 import logo from '@/public/images/logo/short-logo.png'
 import { Button } from "@/components/ui/button"
@@ -14,67 +15,106 @@ import { Label } from "@/components/ui/label"
 import Link from "next/link"
 import Image from "next/image"
 import Multibg from "@/components/multibg"
+import { useCallback, useState } from "react"
+import { useRouter } from "next/navigation"
+import { Eye, EyeOff } from "lucide-react"
+import { SigninAuth } from "@/apis/handleSignin"
 
 export default function Login() {
+    const router = useRouter();
+    const [showPass, setShowPass] = useState(false);
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+        setFormData(prev => ({ ...prev, [id]: value }));
+    }, []);
+    const handleSubmit = useCallback(async (e: React.FormEvent) => {
+        e.preventDefault();
+        const res = await SigninAuth(formData, router);
+
+    }, [formData, router]);
     return (
         <div className="min-h-screen relative flex flex-col">
-            <Multibg/>
+            <Multibg />
             <div className="flex-grow flex flex-col items-center justify-center p-4">
                 <Card className="w-full rounded-md max-w-sm shadow-none relative pt-12">
-                    <div
-                        className="absolute -top-10 left-1/2 -translate-x-1/2 w-17 h-17 rounded-full flex items-center justify-center overflow-hidden">
-                        <div className='cursor-pointer w-[150px] h-[80px]'>
-                            <Image
-                                alt='logo'
-                                src={logo}
-                            />
+                    <form onSubmit={handleSubmit}>
+                        <div
+                            className="absolute -top-10 left-1/2 -translate-x-1/2 w-17 h-17 rounded-full flex items-center justify-center overflow-hidden">
+                            <div className='cursor-pointer w-[150px] h-[80px]'>
+                                <Image
+                                    alt='logo'
+                                    src={logo}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    {/* Card Header */}
-                    <CardHeader>
-                        <CardTitle className="text-2xl text-center mb-2">Sign In</CardTitle>
-                        <CardDescription className="text-center">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, quam. Numquam eligendi soluta.
-                        </CardDescription>
-                    </CardHeader>
-                    {/* Card Content (Form) */}
-                    <CardContent>
-                        <form>
+                        {/* Card Header */}
+                        <CardHeader>
+                            <CardTitle className="text-2xl text-center mb-2">Sign In</CardTitle>
+                            <CardDescription className="text-center">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, quam. Numquam eligendi soluta.
+                            </CardDescription>
+                        </CardHeader>
+                        {/* Card Content (Form) */}
+                        <CardContent>
+
                             <div className="flex flex-col gap-7">
                                 <div className="grid gap-2">
                                     <Label htmlFor="email">Email</Label>
                                     <Input
                                         id="email"
                                         type="email"
-                                        placeholder="m@example.com"
+                                        placeholder="Enter your email"
                                         required
+                                        value={formData.email}
+                                        onChange={handleInputChange}
                                     />
                                 </div>
-                                <div className="grid gap-2">
+                                <div className="relative grid gap-2">
                                     <div className="flex items-center">
                                         <Label htmlFor="password">Password</Label>
                                     </div>
-                                    <Input id="password" placeholder="enter password" type="password" required />
-                                    <Link
-                                        href="/forgotPass"
-                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </Link>
+                                    <div className='absolute bottom-[1px] right-1 rounded-tr-md rounded-br-md flex justify-center items-center  w-10 h-8 bg-white '>
+                                        {
+                                            showPass ?
+                                                <EyeOff onClick={() => setShowPass(false)} className='w-5 text-slate-600 cursor-pointer' />
+                                                :
+                                                <Eye onClick={() => setShowPass(true)} className='w-5 text-slate-600 cursor-pointer' />
+                                        }
+                                    </div>
+                                    <Input id="password" placeholder="Enter password"
+                                        type={`${showPass ? 'text' : 'password'}`}
+                                        required
+                                        value={formData.password}
+                                        onChange={handleInputChange}
+                                    />
                                 </div>
                             </div>
-                        </form>
-                    </CardContent>
-                    {/* Card Footer (Button) */}
-                    <CardFooter className="flex-col gap-2 mt-3">
-                        <Button type="submit" className="w-full bg-blue-800 hover:bg-blue-900">
-                            Sign In
-                        </Button>
-                    </CardFooter>
-                    {/* "New User?" Section */}
-                    <div className="text-center my-3 text-sm ">
-                        <span><span className="text-neutral-400">New User?</span> <Link href={'/register'}>Create and account</Link></span>
-                    </div>
+                            <div className="flex justify-end mt-3">
+                                <Link
+                                    href="/forgotPass"
+                                    className="text-sm underline-offset-4 hover:underline"
+                                >
+                                    Forgot your password?
+                                </Link>
+
+                            </div>
+
+                        </CardContent>
+                        {/* Card Footer (Button) */}
+                        <CardFooter className="flex-col gap-2 mt-3">
+                            <Button type="submit" className="w-full cursor-pointer my-3 bg-blue-800 hover:bg-blue-900">
+                                Sign In
+                            </Button>
+                        </CardFooter>
+                        {/* "New User?" Section */}
+                        <div className="text-center my-3 text-sm ">
+                            <span><span className="text-neutral-400">New User?</span> <Link href={'/register'}>Create and account</Link></span>
+                        </div>
+                    </form>
                 </Card>
                 <Ppts />
             </div>
